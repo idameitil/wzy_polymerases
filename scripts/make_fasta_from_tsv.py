45f5ef543c81ed983aba1f7c50254469a05f12a3
Ida Kristine Sandford Meitil <ida.meitil@gmail.com>
2022-02-03 15:18:43 +0100

    clean-up

--- scripts/make_fasta_from_tsv.py
+++ scripts/make_fasta_from_tsv.py
@@ -1,11 +1,20 @@
 import pandas as pd
+import math
 tsv_file_df = pd.read_csv("data/combined_polymerases.tsv", sep='\t')
-outfile = open("combined_polymerases.fasta", "w")
+outfile = open("data/combined_polymerases.fasta", "w")
 
 for index, row in tsv_file_df.iterrows():
     accession = row['protein_accession']
     species = row['species']
     sequence = row['sequence']
-    outfile.write(f">{accession}|{species}\n{sequence}\n")
+    # Check for nan
+    try:
+        if math.isnan(accession):
+            print('hej')
+            continue
+    except:
+        pass
+    # Save to file
+    outfile.write(f">{accession}\n{sequence}\n")
 
 outfile.close()
