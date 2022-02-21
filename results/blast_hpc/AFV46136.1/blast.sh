960cc9b13c53e649d528869a685aaddc9baf9f73
Ida Kristine Sandford Meitil <ida.meitil@gmail.com>
2022-02-21 10:43:23 +0100

    Added results from blasting annotated Wzys against nr

---
+++ results/blast_hpc/AFV46136.1/blast.sh
@@ -0,0 +1,32 @@
+#! /bin/sh 
+### General options 
+### -- specify queue -- 
+#BSUB -q hpc
+### -- set the job Name -- 
+#BSUB -J blast_AFV46136.1
+### -- ask for number of cores (default: 1) -- 
+#BSUB -n 1 
+### -- specify that the cores must be on the same host -- 
+#BSUB -R "span[hosts=1]"
+### -- specify that we need 2GB of memory per core/slot -- 
+#BSUB -R "rusage[mem=2GB]"
+### -- specify that we want the job to get killed if it exceeds 3 GB per core/slot -- 
+#BSUB -M 3GB
+### -- set walltime limit: hh:mm -- 
+#BSUB -W 2:00 
+### -- set the email address -- 
+# please uncomment the following line and put in your e-mail address,
+# if you want to receive e-mail notifications on a non-default address
+#BSUB -u idamei@dtu.dk
+### -- send notification at start -- 
+#BSUB -B 
+### -- send notification at completion -- 
+#BSUB -N 
+### -- Specify the output and error file. %J is the job-id -- 
+### -- -o and -e mean append, -oo and -eo mean overwrite -- 
+#BSUB -o run/AFV46136.1/jobscript.out
+#BSUB -e run/AFV46136.1/jobscript.err
+# here follow the commands you want to execute 
+export BLASTDB=/work3/garryg//blast/db
+$BLASTDB/../current/bin/blastp -db nr -query run/AFV46136.1/query.fa -out run/AFV46136.1/blast.out
+
